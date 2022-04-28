6b16aced2760ca4b9206c5841e5d2934d0bb945a
Linder47 <levelega47@gmail.com>
2022-04-28 23:43:31 +0300

    Start ignoring .authorization-key, gitignore

--- src/authorization-keys.ts
+++
@@ -1,6 +0,0 @@
-export const REACT_APP_API_KEY = "AIzaSyA5zhWqwZky5PQoi4W2ztOb2nfrseR474o";
-export const REACT_APP_AUTH_DOMAIN = "auth-example-b1339.firebaseapp.com";
-export const REACT_APP_PRJECT_ID = "auth-example-b1339";
-export const REACT_APP_STORAGE_BUCKET = "auth-example-b1339.appspot.com";
-export const REACT_APP_MESSAGING_SENDER_ID = "1088865606243";
-export const REACT_APP_APP_ID = "1:1088865606243:web:5fe3cbc0240144dd043656";
